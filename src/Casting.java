6e113aaa1b18856f53fa8be13074c097151eb945
Bruno Correia <brunocorreia.pessoal10@gmail.com>
2026-07-06 03:43:25 -0300

    Finish lesson

--- src/Casting.java
+++ src/Casting.java
@@ -3,9 +3,5 @@ public class Casting {
         double numeroDouble = 15.7;
         int numeroInteiro = (int) numeroDouble;
         System.out.println("Casting de double para int:" +numeroInteiro);
-
-
-
-
     }
 }
