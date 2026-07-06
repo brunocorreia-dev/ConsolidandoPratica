663643fdf13f648e37eb33e7744139e3f1f42324
Bruno Correia <brunocorreia.pessoal10@gmail.com>
2026-07-06 03:43:18 -0300

    Finish lesson

---
+++ src/StringFuncionario.java
@@ -0,0 +1,10 @@
+public class StringFuncionario {
+    public static void main(String [] args) {
+        String colaborador1;
+        colaborador1 = "bruno";
+        int colaborador1idade = 21;
+        double colaborador1salario = 5600.00;
+
+        System.out.println("nome:" +colaborador1 + ",idade: " +colaborador1idade + ",salario: " +colaborador1salario) ;
+    }
+}
