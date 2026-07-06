80ca032f99bd1741280969e668e0930cb9952e05
Bruno Correia <brunocorreia.pessoal10@gmail.com>
2026-07-06 15:35:24 -0300

    Finish lesson

---
+++ src/Palavras.java
@@ -0,0 +1,13 @@
+public class Palavras {
+    public static void main(String [] args) {
+        char letra = 'C';
+        String palavra = "casa";
+
+        String mensagem = "A letra é " + letra + "e a palavra é: " + palavra;
+
+        System.out.println(mensagem);
+
+
+
+    }
+}
---
+++ src/PrecoProduto.java
@@ -0,0 +1,15 @@
+public class PrecoProduto {
+    public static void main(String[] args) {
+        double precoProduto = 29.99;
+        int quantidade = 3;
+
+        double total = precoProduto * quantidade;
+
+        String mensagem = "O preço total da compra é R$" +total;
+
+        System.out.println(mensagem);
+
+
+
+    }
+}
