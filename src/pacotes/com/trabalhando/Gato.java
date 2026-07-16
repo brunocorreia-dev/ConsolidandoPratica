b04a51e40c0272db20ec02ae99a2a334513aca9f
Bruno Correia <brunocorreia.pessoal10@gmail.com>
2026-07-16 19:19:35 -0300

    feat: evolui modelagem de animais com construtores, encapsulamento e atributos específicos

---
+++ src/pacotes/com/trabalhando/Gato.java
@@ -0,0 +1,29 @@
+package pacotes.com.trabalhando;
+
+public class Gato extends Animal {
+    private String corPelo;
+
+    public Gato(String nome, int idade, String corPelo) {
+        super(nome, idade);
+        this.corPelo = corPelo;
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println(getNome() + " (gato de pelo " + corPelo + ") diz: Miau!");
+    }
+
+    public void arranharMoveis() {
+        System.out.println(getNome() + " começou a arranhar o sofá de novo!");
+    }
+
+
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
+}
+
