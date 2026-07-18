712fe7bf240cd545808ecb8b5fe6dc4880e1862b
Bruno Correia <brunocorreia.pessoal10@gmail.com>
2026-07-18 07:49:09 -0300

    fix(primos): corrige escopo de variável e compilação no arquivo de testes

---
+++ src/pacotes/com/trabalhando/GeradorPrimo.java
@@ -0,0 +1,11 @@
+package pacotes.com.trabalhando;
+
+ class GeradorPrimo extends NumerosPrimos {
+    public int gerarProximoPrimo (int ultimoPrimoConhecido) {
+        int proximoNumero = ultimoPrimoConhecido + 1;
+        while (!verificarPrimalidade(proximoNumero)) {
+            proximoNumero++;
+        }
+        return proximoNumero;
+    }
+}
