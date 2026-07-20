05dd2361aa909f006d891c94d5d77ec0b8ca37c4
Bruno Correia <brunocorreia.pessoal10@gmail.com>
2026-07-20 07:43:21 -0300

    feat: implementa cálculo de vendas para produtos e serviços

---
+++ src/exercicios/Servico/Servico.java
@@ -0,0 +1,17 @@
+package exercicios.Servico;
+
+public class Servico  implements  Vendavel {
+    private String descricao;
+    private double precoHora;
+
+
+    @Override
+    public double calcularPrecoTotal(int quantidade) {
+        return precoHora * quantidade;
+    }
+
+    @Override
+    public void aplicarDesconto(double percentualDesconto) {
+        precoHora -= precoHora * (percentualDesconto / 100.0);
+    }
+}
